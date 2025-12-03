59877cb12f0ff6a6cc3c81ce372223a0ffef0811
Vedali Rane <vedalirane5@gmail.com>
2025-12-03 21:35:49 +0530

    Add files via upload

--- cgpa.java
+++ cgpa.java
@@ -12,7 +12,7 @@ public class cgpa {
     double sum = 0.0;
 
     for(int i=0; i<nbcourses.length; i++){
-        System.out.println("Enter the average for the course"+(i+1)+":");
+        System.out.println("Enter the average for the course  "+(i+1)+":");
         nbcourses[i] = sc.nextDouble();
     }
 
@@ -23,7 +23,18 @@ public class cgpa {
 
     double average = sum / nbcourses.length;
     System.out.println("Your CGPA is: " + average);
-    
+
+    if(average >= 90){
+        System.out.println("You have A grade");
+    }else if(average <= 90 && average >= 80){
+        System.out.println("You have B grade");
+    }else if(average <= 80 && average >= 70){
+        System.out.println("You have C grade");
+    }else if(average <= 70 && average >= 60){
+        System.out.println("You have D grade");
+    }else{
+        System.out.println("You have F grade");
+    }
 
    }
    
