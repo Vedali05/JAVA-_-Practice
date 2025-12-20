47ebfbb0c1905f432b402c7e5049f36964b1d991
Vedali Rane <vedalirane5@gmail.com>
2025-12-20 14:01:31 +0530

    Add files via upload

---
+++ swapwithoutthird.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class swapwithoutthird {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number :");
+        int a = sc.nextInt();
+        System.out.println("Enter the Second Number :");
+        int b = sc.nextInt();
+        System.out.println("Before swapping : a = "+a+" , b = "+b);
+
+        a = a+b;
+        b = a-b;
+        a = a-b;
+
+        System.out.println("After Swapping : a = "+a+" , b = "+b);
+    }
+    
+}
